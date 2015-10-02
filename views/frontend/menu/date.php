0fa6c80baa90a745b32af91453a732f7dd872685
Ryan Mayberry <ryankerkmayberry@gmail.com>
2015-10-02 03:51:10 +0000

    proper url formatting for wp

--- views/frontend/menu/date.php
+++ views/frontend/menu/date.php
@@ -1,5 +1,5 @@
 <div class="well">
     <?foreach($dates as $date) : ?>
-        <a href="<?=$link?>?daterange=<?= str_replace(' ', '', $date); ?>" style="margin-left:1em;"><? echo strtoupper($date) ?></a>
+        <a href="<?= esc_url( add_query_arg( 'daterange',  str_replace(' ', '', $date)  ) ) ?>" style="margin-left:1em;"><? echo strtoupper($date) ?></a>
     <?endforeach?>
 </div>
