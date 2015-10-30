f19c0ad95bb91eda549b4f5024a8a3548fea6d6e
LJ Lee <lj@monaone.(none)>
2015-10-30 21:21:38 +0000

    remove loading.gif if no image found just display item info without image or loading.gif..

--- views/frontend/modal.php
+++ views/frontend/modal.php
@@ -1,33 +1,32 @@
 <? if ($items) : ?>
     <!-- ITEMS LOADED FROM LIVE API CALL -->
     <? foreach ($items as $item)  : ?>
-
-        <div class="modal fade" id="myModal-<?= $item->objectId ?>" imgsrc="<?= $item->imageUrl() ?>">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                        <h2 class="modal-title"><? if ($item->title) : ?><?= $item->title ?><? else: ?> Untitled<? endif ?>
-                        </h2>
-                    </div>
-
-                    <div class="modal-body">
-                        <div class="imageWrap">
-                            <img id="img-myModal-<?= $item->objectId ?>" class="mainImage center-block"/>
+            <div class="modal fade" id="myModal-<?= $item->objectId ?>" imgsrc="<?= $item->imageUrl() ?>">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                            <h2 class="modal-title"><? if ($item->title) : ?><?= $item->title ?><? else: ?> Untitled<? endif ?>
+                            </h2>
                         </div>
 
-                        <? Flight::view()->render('frontend/imageBox', array('item' => $item)) ?>
-                        <? Flight::view()->render('frontend/template/listing', array('item' => $item)) ?>
+                        <div class="modal-body">
+                            <div class="imageWrap">
+                                <img id="img-myModal-<?= $item->objectId ?>" class="mainImage center-block"/>
+                            </div>
 
+                            <? Flight::view()->render('frontend/imageBox', array('item' => $item)) ?>
+                            <? Flight::view()->render('frontend/template/listing', array('item' => $item)) ?>
+
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                        </div>
                     </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                    </div>
+
+
                 </div>
-
-
             </div>
-        </div>
     <? endforeach ?>
 <? endif ?>
 
@@ -37,8 +36,12 @@
         $('#myModal').modal();
         $('.modal').on('shown.bs.modal', function (e) {
             var imgsrc = $(this).attr('imgsrc');
-            $('#img-' + $(this).attr('id')).attr('src', imgsrc);
-            console.log(imgsrc);
+            if(imgsrc){
+                $('#img-' + $(this).attr('id')).attr('src', imgsrc);
+            }
+            else{
+                $("#" + $(this).attr('id') + " .imageWrap").hide();
+            }
         })
     });
 </script>
