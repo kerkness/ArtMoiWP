9c1bdeac93e4dad195a20a21d114fbdf23b69861
LJ Lee <lj@monaone.(none)>
2015-11-02 21:22:05 +0000

    Remove loading.gif if no image found in single.php / change loading.gif

--- views/frontend/template/single.php
+++ views/frontend/template/single.php
@@ -3,9 +3,11 @@
     <div class="clearfix"></div>
     <h2><?if($item->title) : ?><?=$item->title?><? else : ?>Untitled<?endif?></h2>
     <div class="col-md-5 col-xs-12">
+        <? if($item->imageUrl()) : ?>
         <div class="imageWrap center-block row">
             <img src="<?=$item->imageUrl()?>" class="mainImage img-responsive">
         </div>
+        <?endif?>
     </div>
     <div class="col-md-6 col-xs-12">
         <? Flight::view()->render('frontend/imageBox', array('item' => $item)) ?>
